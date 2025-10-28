3e35bf6fdfc5e50523cb63ce8d72f88520b4dbd7
LucasSantosCarlos <lucassantosott@gmail.com>
2025-10-28 18:20:23 -0300

    Primeiro commit - estrutura base Spring Boot

---
+++ src/main/java/com/example/palmilha_backend/PalmilhaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.palmilha_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmilhaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalmilhaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/palmilha_backend/PalmilhaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.palmilha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmilhaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
